4af44c1fb18f8e96f42654b295e9416c176d62bb
Harrison Lingren <harrisonlingren@gmail.com>
2015-02-27 20:33:14 -0500

    update to current 150227833

--- WumpusDriver.java
+++ WumpusDriver.java
@@ -13,6 +13,8 @@ class WumpusDriver
   int pitRoom2;
   int spidRoom1;
   int spidRoom2;
+  int batsRoom;
+  int supplyRoom;
   
   Scanner input = new Scanner(System.in);
 	
@@ -33,15 +35,63 @@ class WumpusDriver
 		// generate random numbers to assign the wumpus, 2 pits,
 		// and 2 spiders into rooms
 		
-		Random generate = new Random();
+		// wumpRoom=generate.nextInt(caveStructure.length)+1;
+		// pitRoom1=generate.nextInt(caveStructure.length)+1;
+		// pitRoom2=generate.nextInt(caveStructure.length)+1;
+		// spidRoom1=generate.nextInt(caveStructure.length)+1;
+		// spidRoom2=generate.nextInt(caveStructure.length)+1;
 		
-		wumpRoom=generate.nextInt(caveStructure.length);
-		pitRoom1=generate.nextInt(caveStructure.length);
-		pitRoom2=generate.nextInt(caveStructure.length);
-		spidRoom1=generate.nextInt(caveStructure.length);
-		spidRoom2=generate.nextInt(caveStructure.length);
+		int[] roomAssign = new int[caveStructure.length];
+		for(int i=0; i<caveStructure.length-1; i++)
+      {
+        roomAssign[i]=i+2;
+        System.out.print(i+"="+roomAssign[i]+", ");
+      }
+    System.out.println();
+		roomAssign=RandomizeArray(roomAssign);
+		
+		wumpRoom=roomAssign[1];
+    pitRoom1=roomAssign[2];
+    pitRoom2=roomAssign[3];
+    spidRoom1=roomAssign[4];
+    spidRoom2=roomAssign[5];
+    batsRoom=roomAssign[6];
+    supplyRoom=roomAssign[7];
+	}
+  
+  // shuffle
+  /*static void shuffleArray(int[] array)
+  {
+    Random rand = new Random();
+    for (int i=array.length-1; i>0; i--)
+    {
+      int index=rand.nextInt(i);
+      // swap
+      int a=array[index];
+      array[index]=array[i];
+      array[i]=a;
+      System.out.print(i+"="+array[i]+", ");
+    }
+  }*/
+  
+  public static int[] RandomizeArray(int[] array){
+    Random rgen = new Random();  // Random number generator      
+ 
+    for (int i=0; i<array.length-2; i++) {
+        int randomPosition = rgen.nextInt(array.length);
+        int temp = array[i];
+        array[i] = array[randomPosition];
+        array[randomPosition] = temp;
+    }
+    
+    for(int i=0; i<array.length; i++)
+    {
+      System.out.print(i+"="+array[i]+", ");
+    }
+    
+    return array;
   }
-   
+  
   public void startGame()
 		throws IOException, InterruptedException
   {
@@ -49,33 +99,34 @@ class WumpusDriver
     arrowCount=3;
     currentRoom=1;
      
-	//prints ASCII title
+	  //prints ASCII title
 	
-	System.out.print("Welcome to");
-	Thread.sleep(250);
-	System.out.print(".");
-	Thread.sleep(250);
-  System.out.print(".");
-  Thread.sleep(250);
-  System.out.print(".");
-  Thread.sleep(250);
+	
+  	System.out.print("Welcome to");
+  	Thread.sleep(250);
+  	System.out.print(".");
+  	Thread.sleep(250);
+    System.out.print(".");
+    Thread.sleep(250);
+    System.out.print(".");
+    Thread.sleep(250);
       
-	System.out.println();			
-	System.out.println("                    _     _   _            __    __                                  ");
-	Thread.sleep(50);
-	System.out.println("  /\\  /\\_   _ _ __ | |_  | |_| |__   ___  / / /\\ \\ \\_   _ _ __ ___  _ __  _   _ ___ TM");
-	Thread.sleep(50);
-	System.out.println(" / /_/ / | | | '_ \\| __| | __| '_ \\ / _ \\ \\ \\/  \\/ / | | | '_ ` _ \\| '_ \\| | | / __| ");
-	Thread.sleep(50);
-	System.out.println("/ __  /| |_| | | | | |_  | |_| | | |  __/  \\  /\\  /| |_| | | | | | | |_) | |_| \\__ \\ ");
-	Thread.sleep(50);
-	System.out.println("\\/ /_/  \\__,_|_| |_|\\__|  \\__|_| |_|\\___|   \\/  \\/  \\__,_|_| |_| |_| .__/ \\__,_|___/ ");
-	Thread.sleep(50);
-	System.out.println("                                                                   |_|               ");
-	Thread.sleep(250);
-			
-			
+  	System.out.println();			
+  	System.out.println("                    _     _   _            __    __                                  ");
+  	Thread.sleep(50);
+  	System.out.println("  /\\  /\\_   _ _ __ | |_  | |_| |__   ___  / / /\\ \\ \\_   _ _ __ ___  _ __  _   _ ___ TM");
+  	Thread.sleep(50);
+  	System.out.println(" / /_/ / | | | '_ \\| __| | __| '_ \\ / _ \\ \\ \\/  \\/ / | | | '_ ` _ \\| '_ \\| | | / __| ");
+  	Thread.sleep(50);
+  	System.out.println("/ __  /| |_| | | | | |_  | |_| | | |  __/  \\  /\\  /| |_| | | | | | | |_) | |_| \\__ \\ ");
+  	Thread.sleep(50);
+  	System.out.println("\\/ /_/  \\__,_|_| |_|\\__|  \\__|_| |_|\\___|   \\/  \\/  \\__,_|_| |_| |_| .__/ \\__,_|___/ ");
+  	Thread.sleep(50);
+  	System.out.println("                                                                   |_|               ");
+  	Thread.sleep(250);
+		
     startTurn(currentRoom, caveStructure);
+    
   }
 
 	public boolean wumpCheck(int r)
@@ -96,6 +147,18 @@ class WumpusDriver
 	  else {return false;}
 	}
 	
+	public boolean supplyCheck(int r)
+	{
+	  if(r==supplyRoom) {return true;}
+	  else {return false;}
+	}
+	
+	public boolean batCheck(int r)
+	{
+	  if(r==batsRoom) {return true;}
+	  else {return false;}
+	}
+	
 	// 
   public void startTurn(int r, Room[] cave)
 		throws IOException, InterruptedException
@@ -110,15 +173,19 @@ class WumpusDriver
     System.out.println("You are in room "+r+".");
     // print the arrow count
     System.out.println("You have "+arrowCount+" arrows left.");
+    System.out.println();
     // print the room scenario
     System.out.println(cave[r-1].scenario);
     // print the adjacent rooms
     System.out.println("There are tunnels to rooms "+cave[r-1].adj1+", "+cave[r-1].adj2+", and "+cave[r-1].adj3);
-    
-    // System.out.println( "wumpus:"+wumpRoom+wumpCheck()+" pits:"+pitRoom1+":"+pitRoom2+pitCheck()+" spider:"+spidRoom1+":"+spidRoom2+spiderCheck() );
+    // for debugging
+    System.out.println( "wumpus:"+wumpRoom+wumpCheck(r)+" pits:"+pitRoom1+","+pitRoom2+":"+pitCheck(r)+" spider:"+spidRoom1+","+spidRoom2+":"+spiderCheck(r)+" bats:"+batsRoom+":"+batCheck(r)+" supply:"+supplyRoom+":"+supplyCheck(r) );
 		
-  	// run wumpCheck(), spiderCheck(), and pitCheck() to check for dangers
-  	// after they are distributed in setRooms()
+  
+		if(wumpCheck(cave[r-1].adj1) || wumpCheck(cave[r-1].adj2) || wumpCheck(cave[r-1].adj3) )
+		  { System.out.println("You smell some nasty Wumpus!"); }
+		  
+		
 		
   	//prompt for action
   	System.out.println();
@@ -141,7 +208,7 @@ class WumpusDriver
 	  }
   }
   
-  // 
+   
   public void move(int r, Room[] cave)
     throws IOException, InterruptedException
   {
@@ -173,14 +240,17 @@ class WumpusDriver
     if (wumpCheck(r) )
     {
       gameOver("The Wumpus got you!");
+      return;
     }
     else if(pitCheck(r) )
     {
       gameOver("You fell into a bottomless pit!");
+      return;
     }
     else if(spiderCheck(r) )
     {
       gameOver("Spiders attacked you!");
+      return;
     }
     
     startTurn(r, cave);
@@ -209,25 +279,28 @@ class WumpusDriver
     
     if(ansNum==(cave[r-1].adj1) ) 
     {
-      if( wumpCheck(r) )
+      if( wumpCheck(cave[r-1].adj1) )
       {
         winGame();
+        return;
       }
     }
     
     else if(ansNum==(cave[r-1].adj2) )
     {
-      if( wumpCheck(r) )
+      if( wumpCheck(cave[r-1].adj2) )
       {
         winGame();
+        return;
       }
     }
     
     else if(ansNum==(cave[r-1].adj3) ) 
     {
-      if( wumpCheck(r) )
+      if( wumpCheck(cave[r-1].adj3) )
       {
         winGame();
+        return;
       }
     }
     
@@ -248,15 +321,28 @@ class WumpusDriver
   }
   
   public void winGame()
+    throws IOException, InterruptedException
   {
+    String ans;
+    
+    System.out.println("Congratulations! You win!");
+    Thread.sleep(500);
+    System.out.println("Do you want to play again? (y/n)");
+    
+    ans=input.next();
+    
+    if(ans.equals("y") ) {startGame();}
     
   }
   
   public void gameOver(String reason)
+    throws InterruptedException
   {
     
     System.out.println("Oh no!");
     System.out.println(reason);
+    Thread.sleep(1000);
+    System.out.println("GAME OVER...");
     
   }
 
